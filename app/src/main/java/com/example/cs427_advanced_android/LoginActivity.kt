47b3aa663f97d9068f5a5ba5a719ab8ae0910825
Youssef Mohamed <81262678+yousseffninja@users.noreply.github.com>
2023-02-21 01:23:26 +0200

    Login and registration using firebase

--- app/src/main/java/com/example/cs427_advanced_android/LoginActivity.kt
+++ app/src/main/java/com/example/cs427_advanced_android/LoginActivity.kt
@@ -1,11 +1,60 @@
 package com.example.cs427_advanced_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        val RegisterNav: TextView = findViewById(R.id.RegisterNav)
+
+        val Login: Button = findViewById(R.id.buttonLogin)
+
+        RegisterNav.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        Login.setOnClickListener {
+            performLogin();
+        }
     }
+
+    private fun performLogin() {
+        val EmailText: EditText = findViewById(R.id.editTextTextEmailAddress)
+        val PasswordText: EditText = findViewById(R.id.editTextTextPassword)
+
+        val inputEmail = EmailText.text.toString();
+        val inputPassword = PasswordText.text.toString();
+
+        auth.signInWithEmailAndPassword(inputEmail, inputPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Authentication Success.",
+                        Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,HomeActivity::class.java))
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/example/cs427_advanced_android/RegisterActivity.kt
+++ app/src/main/java/com/example/cs427_advanced_android/RegisterActivity.kt
@@ -1,11 +1,87 @@
 package com.example.cs427_advanced_android
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private var db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = Firebase.auth
+
+        val LoginNav: TextView = findViewById(R.id.rlog)
+        val Register: Button = findViewById(R.id.buttonRegister)
+
+        LoginNav.setOnClickListener {
+            finish()
+        }
+
+        Register.setOnClickListener {
+            performSignUp();
+        }
+    }
+
+    private fun performSignUp() {
+        val firstName: EditText = findViewById(R.id.firstname)
+        val lastName: EditText = findViewById(R.id.lastname)
+        val email: EditText = findViewById(R.id.email)
+        val password: EditText = findViewById(R.id.passowrd)
+        val confirmPassword: EditText = findViewById(R.id.confirm_password)
+
+        val inputFirstName = firstName.text.toString()
+        val inputInputLastName = lastName.text.toString()
+        val inputEmail = email.text.toString()
+        val inputPassword = password.text.toString()
+        val inputConfirmPassword = confirmPassword.text.toString()
+
+        if (inputPassword == inputConfirmPassword){
+            auth.createUserWithEmailAndPassword(inputEmail, inputPassword)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        CreateUser(inputFirstName, inputInputLastName, inputEmail)
+                        Toast.makeText(this, "Authentication Success\n Please Login", Toast.LENGTH_SHORT).show()
+                        finish();
+                    } else {
+                        // If sign in fails, display a message to the user.
+
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    private fun CreateUser(inputFirst: String, inputLast: String,
+                           inputEmail: String) {
+        val user = hashMapOf(
+            "first name" to inputFirst,
+            "last last" to inputLast,
+            "email" to inputEmail
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
