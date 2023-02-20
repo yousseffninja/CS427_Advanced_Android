08b9925a52439a08f1f11098f30c2c9ad50cc401
Youssef Mohamed <81262678+yousseffninja@users.noreply.github.com>
2023-02-20 23:30:20 +0200

    Add splash screen update gradle update android

---
+++ app/src/main/java/com/example/cs427_advanced_android/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.cs427_advanced_android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed( {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+
+    }
+}
