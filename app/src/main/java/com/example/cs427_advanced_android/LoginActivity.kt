ef3b0ddd456cbbfed73980f04cee2c099fc0920a
Youssef Mohamed <81262678+yousseffninja@users.noreply.github.com>
2023-03-28 01:39:02 +0200

    Notification Google Sign in with firebase(Still Error !)

--- app/src/main/java/com/example/cs427_advanced_android/LoginActivity.kt
+++ app/src/main/java/com/example/cs427_advanced_android/LoginActivity.kt
@@ -1,14 +1,27 @@
 package com.example.cs427_advanced_android
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import com.example.cs427_advanced_android.notifications.NotificationActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.GlobalScope
@@ -16,26 +29,130 @@ import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val RC_SIGN_IN: Int = 1
+    lateinit var gso: GoogleSignInOptions
+    private lateinit var mAuth: FirebaseAuth
+    @SuppressLint("UnspecifiedImmutableFlag")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var notificationChannel: NotificationChannel
+        lateinit var builder: Notification.Builder
+        val channelId = "i.apps.notifications"
+        val description = "Test notification"
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        auth = Firebase.auth
+//        auth = Firebase.auth
+        mAuth = FirebaseAuth.getInstance()
+
+        createRequest();
 
         val RegisterNav: TextView = findViewById(R.id.RegisterNav)
 
         val Login: Button = findViewById(R.id.buttonLogin)
 
+        val sGoogle: Button = findViewById(R.id.SignGoogle)
+
+        val Notify: Button = findViewById(R.id.button_notify)
+
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         RegisterNav.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        Notify.setOnClickListener {
+
+            val intent = Intent(this, NotificationActivity::class.java)
+//            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val contentView = RemoteViews(packageName, R.layout.activity_notification)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this, channelId)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+//                    .setContentIntent(pendingIntent)
+            } else {
+
+                builder = Notification.Builder(this)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+//                    .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(1234, builder.build())
         }
 
         Login.setOnClickListener {
             performLogin();
         }
+
+        sGoogle.setOnClickListener {
+            signIn();
+        }
     }
 
+    private fun createRequest() {
+        // Configure Google Sign In
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception=task.exception
+
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account)
+            }
+            catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, "Login Failed1", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent= Intent(this,HomeActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@LoginActivity, "Login Failed2: ", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
     private fun performLogin() {
         val EmailText: EditText = findViewById(R.id.editTextTextEmailAddress)
         val PasswordText: EditText = findViewById(R.id.editTextTextPassword)
@@ -43,7 +160,7 @@ class LoginActivity : AppCompatActivity() {
         val inputEmail = EmailText.text.toString();
         val inputPassword = PasswordText.text.toString();
 
-        auth.signInWithEmailAndPassword(inputEmail, inputPassword)
+        mAuth.signInWithEmailAndPassword(inputEmail, inputPassword)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Toast.makeText(this, "Authentication Success.",
---
+++ app/src/main/java/com/example/cs427_advanced_android/notifications/NotificationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cs427_advanced_android.notifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cs427_advanced_android.R
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
